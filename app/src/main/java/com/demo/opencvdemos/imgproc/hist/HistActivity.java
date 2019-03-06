845448b76b6dbd1a03af7ef745357fdea4804ced
wangyt <wangyt_zzu@163.com>
2019-03-06 09:42:52 +0800

    直方图计算

--- app/src/main/java/com/demo/opencvdemos/imgproc/hist/HistActivity.java
+++ app/src/main/java/com/demo/opencvdemos/imgproc/hist/HistActivity.java
@@ -15,17 +15,24 @@ import android.widget.Toast;
 import com.demo.opencvdemos.R;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int REQ_CODE_PICK_IMG = 1;
 
-    Button btnLoadImg, btnHistEqu;
+    Button btnLoadImg, btnHistEqu, btnHistCal;
     ImageView imgSrc, imgDst;
 
     Mat src;
@@ -38,9 +45,11 @@ public class HistActivity extends AppCompatActivity implements View.OnClickListe
 
         btnLoadImg = findViewById(R.id.btnLoadImg);
         btnHistEqu = findViewById(R.id.btnHistEqu);
+        btnHistCal = findViewById(R.id.btnHistCal);
 
         btnLoadImg.setOnClickListener(this);
         btnHistEqu.setOnClickListener(this);
+        btnHistCal.setOnClickListener(this);
 
         imgSrc = findViewById(R.id.imgSrc);
         imgDst = findViewById(R.id.imgDst);
@@ -66,13 +75,16 @@ public class HistActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnHistEqu:
                 dst = histEqualize();
                 break;
+            case R.id.btnHistCal:
+                dst = histCalculation();
+                break;
         }
 
         //显示输出图像
         showDst(dst);
     }
 
-    private Mat histEqualize(){
+    private Mat histEqualize() {
         //灰度化
         Mat grayMat = new Mat();
         Imgproc.cvtColor(src, grayMat, Imgproc.COLOR_BGR2GRAY);
@@ -80,6 +92,49 @@ public class HistActivity extends AppCompatActivity implements View.OnClickListe
         Mat histEquMat = new Mat();
         Imgproc.equalizeHist(grayMat, histEquMat);
         return histEquMat;
+    }
+
+    private Mat histCalculation(){
+        //分离源图像颜色面
+        List<Mat> bgrPlanes = new ArrayList<>();
+        Core.split(src, bgrPlanes);
+        //set bins size
+        int histSize = 256;
+        //设定像素值范围,不含上界
+        float range[] = {0,256};
+        MatOfFloat histRange = new MatOfFloat(range);
+        //计算直方图
+        Mat bHist = new Mat(), gHist = new Mat(), rHist = new Mat();
+        Imgproc.calcHist(bgrPlanes, new MatOfInt(0), new Mat(), bHist,
+                new MatOfInt(histSize), histRange, false);
+        Imgproc.calcHist(bgrPlanes, new MatOfInt(1), new Mat(), gHist,
+                new MatOfInt(histSize), histRange, false);
+        Imgproc.calcHist(bgrPlanes, new MatOfInt(2), new Mat(), rHist,
+                new MatOfInt(histSize), histRange, false);
+        //初始化直方图底图
+        int histW = 500, histH = 500;
+        int binW = (int) Math.round((double) histW / histSize);
+        Mat histImage = new Mat(histH, histW, CvType.CV_8UC3, new Scalar(0,0,0));
+        //规范化直方图
+        Core.normalize(bHist, bHist, 0, histImage.rows(), Core.NORM_MINMAX);
+        Core.normalize(gHist, gHist, 0, histImage.rows(), Core.NORM_MINMAX);
+        Core.normalize(rHist, rHist, 0, histImage.rows(), Core.NORM_MINMAX);
+        //绘制直方图
+        float[] bHistData = new float[(int) (bHist.total() * bHist.channels())];
+        bHist.get(0, 0, bHistData);
+        float[] gHistData = new float[(int) (gHist.total() * gHist.channels())];
+        gHist.get(0, 0, gHistData);
+        float[] rHistData = new float[(int) (rHist.total() * rHist.channels())];
+        rHist.get(0, 0, rHistData);
+        for( int i = 1; i < histSize; i++ ) {
+            Imgproc.line(histImage, new Point(binW * (i - 1), histH - Math.round(bHistData[i - 1])),
+                    new Point(binW * (i), histH - Math.round(bHistData[i])), new Scalar(255, 0, 0), 2);
+            Imgproc.line(histImage, new Point(binW * (i - 1), histH - Math.round(gHistData[i - 1])),
+                    new Point(binW * (i), histH - Math.round(gHistData[i])), new Scalar(0, 255, 0), 2);
+            Imgproc.line(histImage, new Point(binW * (i - 1), histH - Math.round(rHistData[i - 1])),
+                    new Point(binW * (i), histH - Math.round(rHistData[i])), new Scalar(0, 0, 255), 2);
+        }
+        return histImage;
     }
 
     private void loadImg() {
